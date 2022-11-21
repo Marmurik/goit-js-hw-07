4f8d6506c825a865b58c5c55ab4239a6222b38c9
Anastasia Marmura <anastasia.marmura@gmail.com>
2022-11-21 19:44:57 +0200

    task1_re-rendered markup

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,25 +2,47 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 console.log(galleryItems);
 
-
 const gallery = document.querySelector('.gallery')
-const items = []
 
-galleryItems.forEach(item => {
-   gallery.classList.add('gallery__item');
-   const galleryLink = document.createElement('a');
-   galleryLink.classList.add('gallery__link');
-   const imgEl = document.createElement('img');
-   imgEl.classList.add('gallery__image');
-   imgEl.src = item.preview;
-   imgEl.dataset.source = item.original;
-   imgEl.alt = item.description;
+const GalleryMarkup = createGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', GalleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems.map(
+  ({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+      <a class="gallery__item"
+        href='${original}'>
+        <img class='gallery__image'
+            src='${preview}' 
+            data-source='${original}'
+            alt='${description}'/>
+      </a>
+    </div>
+        `;
+    })
+    .join("");
+}
+
+// const gallery = document.querySelector('.gallery')
+// const items = []
+
+// galleryItems.forEach(item => {
+//    gallery.classList.add('gallery__item');
+//    const galleryLink = document.createElement('a');
+//    galleryLink.classList.add('gallery__link');
+//    const imgEl = document.createElement('img');
+//    imgEl.classList.add('gallery__image');
+//    imgEl.src = item.preview;
+//    imgEl.dataset.source = item.original;
+//    imgEl.alt = item.description;
 
 
-   galleryLink.append(imgEl);
-   items.push(galleryLink);
-})
-gallery.append(...items); 
+//    galleryLink.append(imgEl);
+//    items.push(galleryLink);
+// })
+// gallery.append(...items); 
 
 gallery.addEventListener('click', onImageClick)
     
@@ -30,7 +52,7 @@ function onImageClick(e) {
     return;
 } 
     const instance = basicLightbox.create(
-    `<img src = "${e.target.dataset.source}" width="1280" height="853">`
+    `<img src = "${e.target.dataset.source}" width="800" height="600">`
   );
 
   instance.show();
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -12,8 +12,8 @@ function createGalleryMarkup(galleryItems) {
   ({ preview, original, description }) => {
       return `
         <a class="gallery__item"
-        href="${original}">
-        <img class="gallery__image"
+        href='${original}'>
+        <img class='gallery__image'
             src='${preview}' 
             alt='${description}'/>
         </a>
@@ -28,28 +28,5 @@ const lightBox = new SimpleLightbox('.gallery__item', {
 });
 
 
-// // const gallery = document.querySelector('.gallery')
-// // const items = []
 
-// // galleryItems.forEach(item => {
-
-// //    const galleryLink = document.createElement('a');
-// //    galleryLink.classList.add('gallery__item');
-   
-// //    const imgEl = document.createElement('img');
-// //    imgEl.classList.add('gallery__image');
-// //    imgEl.src = item.preview;
-// //    imgEl.alt = item.description;
-// //    imgEl.href = item.original;
-
-
-// //    galleryLink.append(imgEl);
-// //    items.push(galleryLink);
-// // })
-// // gallery.append(...items); 
-
-//   new SimpleLightbox('.gallery a', {
-//     captionDelay: 250,
-//     captionsData: 'alt',
-//   });
 
